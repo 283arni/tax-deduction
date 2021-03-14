0ee39c7817d369e1eccf4f7d195a4b725c7f7c54
Aleksandr Arnautov <283arni@gmail.com>
2021-03-14 23:34:49 +0300

    Добавляет логику к input

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,10 +1,30 @@
 import classes from './Popup.module.scss';
+import {useState} from 'react';
 import Button from "../Button/Button";
 import CloseButton from "../CloseButton/CloseButton";
 import Tags from "../Tags/Tags";
 import Deductions from "../Deductions/Deductions";
 
+const transformSalary = (sum) => {
+
+  sum = parseInt(sum.split(` `).join(``).replace(`₽`,''), 10);
+
+  if (Number.isNaN(sum)) {
+    return '';
+  }
+
+  return `${sum.toString().replace(/(.{3})/g, "$1 ")} ₽`
+};
+
 const Popup = ({isOpen, onTogglePopupClick}) => {
+  const [salary, setSalary] = useState('')
+
+  const handleFieldChange = (e) => {
+    const value = transformSalary(e.target.value);
+
+    setSalary(value);
+  }
+
   return (
     <div className={classes.Popup}>
        <div className={classes.Popup__container}>
@@ -16,7 +36,14 @@ const Popup = ({isOpen, onTogglePopupClick}) => {
            <p>Используйте налоговый вычет чтобы погасить ипотеку досрочно. Размер налогового вычета составляет не более 13% от своего официального годового дохода.</p>
            <div className={classes.Popup__calculate}>
              <label htmlFor="salary">Ваша зарплата в месяц</label>
-             <input type="text" id="salary" placeholder="Введите данные"/>
+             <input
+               onChange={(e) => handleFieldChange(e)}
+               type="text"
+               id="salary"
+               placeholder="Введите данные"
+               maxLength={13}
+               value={salary}
+             />
              <button type="button">Рассчитать</button>
            </div>
            <Deductions/>
